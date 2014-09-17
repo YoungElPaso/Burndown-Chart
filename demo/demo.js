d7770bc28aaf94b55417d44a6ff113b5f8983a01
Jesse Sutherland <jesse.sutherland@mcgill.ca>
2014-09-17 15:52:27 -0400

    added code from original jsFiddle, to the demo files, css, html, js, .details and a better description

---
+++ demo/demo.js
@@ -0,0 +1,35 @@
+// Get data from a table.
+var rawData = [];
+var table;
+table = $('.iteration-report table');
+$.each(table.find('tbody').find('tr'), function (e) {
+    var row = {};
+    row.date = $(this).find('td.date').text();
+    row.actual = $(this).find('td.actual').text() ? $(this).find('td.actual').text() : null;
+    row.objectives = $(this).find('td.objectives').text() ? $(this).find('td.objectives').text() : null;
+    row.objectivesWBuffer = $(this).find('td.objectives_w_buffer').text() ? $(this).find('td.objectives_w_buffer').text() : null;
+    rawData.push({
+        'd': row.date,
+            'actual': row.actual,
+            'objectives': row.objectives,
+            'objectivesWBuffer': row.objectivesWBuffer
+    });
+});
+
+// Basic line graph.
+Morris.Line({
+    element: 'burn-down',
+    data: rawData,
+    xkey: 'd',
+    xLabels: 'day',
+    ykeys: ['actual', 'objectives', 'objectivesWBuffer'],
+    labels: ['Open SP', 'Objectives', 'Objectives Incl. Buffer'],
+    smooth: false,
+    hideHover: false,
+    lineColors: ['orange', 'lightblue', 'Gainsboro'],
+    lineWidth: 2,
+    pointSize: 3,
+    pointFillColors: ['Red', 'White', 'Silver'],
+    ymax:70,
+    numLines: 8, //start+buffer+1 / 10 *rounded for base 10
+});
